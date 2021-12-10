81b01d108ffb9f3c7c0093421d9a39f90aa484c5
Bodleum <git@daniellaing.com>
2021-12-10 16:13:30 +0000

    Fixed `termcmd` bug

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
-#define TERMINAL "Alacritty"
+#define TERMINAL "alacritty"
 #define TERMCLASS "Alacritty"
 
 /* appearance */
@@ -90,7 +90,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { TERMINAL, NULL };
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
