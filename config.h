d903cc8f71fc7e8933602b7b0bb8558decbe7c6e
Bodleum <git@daniellaing.com>
2021-12-14 19:28:42 +0000

    Keybind changes

--- config.h
+++ config.h
@@ -214,7 +214,7 @@ static Key keys[] = {
     //{ MODKEY|ShiftMask,             XK_z,           spawn,          SHCMD("") },
     { MODKEY,                       XK_x,           incrgaps,       {.i = -3} },    // Decrease gaps
     //{ MODKEY|ShiftMask,             XK_x,           spawn,          SHCMD("") },
-    //{ MODKEY,                       XK_c,           spawn,          SHCMD("") },
+    { MODKEY,                       XK_c,           spawn,          SHCMD(TERMINAL " -e calcurse") },   // Open Calendar
     { MODKEY|ShiftMask,             XK_c,           spawn,          SHCMD("mpv --no-cache --no-osc --no-input-default-bindings --profile=low-latency --input-conf=/dev/null --title=Webcam $(/bin/ls /dev/video[0,2,4,6,8] | tail -n 1)") },    // Open webcam
     /*
         V is bound in STACKKEYS
@@ -232,6 +232,7 @@ static Key keys[] = {
 
     { MODKEY,                       XK_space,       zoom,           {0} },  // Make current window master
     { MODKEY|ShiftMask,             XK_space,       togglefloating, {0} },  // Toggle floating
+    { Mod1Mask,                     XK_space,       spawn,          SHCMD("dmenu_run") },  // Launch a program
 
     { MODKEY,                       XK_Return,      spawn,          {.v = termcmd} },   // Open terminal
     { MODKEY|ShiftMask,             XK_Return,      togglescratch,  {.ui = 0} },    // Floating terminal
