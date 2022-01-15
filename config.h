3005a86a467d53ab3b78f3ff004d9166eb452646
Bodleum <daniel@daniellaing.com>
2022-01-15 23:11:08 +0000

    Partial support of fn media keys

--- config.h
+++ config.h
@@ -174,6 +174,7 @@ static const Layout layouts[] = {
 /* commands */
 static const char *termcmd[]  = { TERMINAL, NULL };
 
+#include <X11/XF86keysym.h>
 
 static Key keys[] = {
     /* modifier                     key             function        argument */
@@ -297,6 +298,36 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_Left,        tagmon,         {.i = -1 } },   // Move window to monitor to the left
     { MODKEY,                       XK_Right,       focusmon,       {.i = +1 } },   // Focus on monitor to the right
     { MODKEY|ShiftMask,             XK_Right,       tagmon,         {.i = +1 } },   // Move window to monitor to the right
+
+
+    { 0, XF86XK_AudioMute,          spawn,                          SHCMD("pamixer -t; kill -39 $(pidof dwmblocks)") },
+    { 0, XF86XK_AudioRaiseVolume,   spawn,                          SHCMD("pamixer --allow-boost -i 3; kill -39 $(pidof dwmblocks)") },
+    { 0, XF86XK_AudioLowerVolume,   spawn,                          SHCMD("pamixer --allow-boost -d 3; kill -39 $(pidof dwmblocks)") },
+    { 0, XF86XK_AudioPrev,          spawn,                          SHCMD("mpc prev") },
+    { 0, XF86XK_AudioNext,          spawn,                          SHCMD("mpc next") },
+    { 0, XF86XK_AudioPause,         spawn,                          SHCMD("mpc pause") },
+    { 0, XF86XK_AudioPlay,          spawn,                          SHCMD("mpc play") },
+    { 0, XF86XK_AudioStop,          spawn,                          SHCMD("mpc stop") },
+    { 0, XF86XK_AudioRewind,        spawn,                          SHCMD("mpc seek -10") },
+    { 0, XF86XK_AudioForward,       spawn,                          SHCMD("mpc seek +10") },
+    { 0, XF86XK_AudioMedia,         spawn,                          SHCMD(TERMINAL " -e ncmpcpp") },
+    { 0, XF86XK_AudioMicMute,       spawn,                          SHCMD("pactl set-source-mute @DEFAULT_SOURCE@ toggle") },
+    { 0, XF86XK_PowerOff,           spawn,                          SHCMD("system_action.sh") },
+    { 0, XF86XK_Calculator,         spawn,                          SHCMD(TERMINAL " -e bc -l") },
+    { 0, XF86XK_Sleep,              spawn,                          SHCMD("system_action.sh") },
+    { 0, XF86XK_WWW,                spawn,                          SHCMD("$BROWSER") },
+    { 0, XF86XK_DOS,                spawn,                          SHCMD(TERMINAL) },
+    { 0, XF86XK_ScreenSaver,        spawn,                          SHCMD("slock & xset dpms force off; mpc pause; pauseallmpv") },
+    { 0, XF86XK_TaskPane,           spawn,                          SHCMD(TERMINAL " -e bpytop") },
+    { 0, XF86XK_Mail,               spawn,                          SHCMD(TERMINAL " -e neomutt ; pkill -RTMIN+3 dwmblocks") },
+    { 0, XF86XK_MyComputer,         spawn,                          SHCMD(TERMINAL " -e lf /") },
+    { 0, XF86XK_Battery,            spawn,                          SHCMD("") },
+    { 0, XF86XK_Launch1,            spawn,                          SHCMD("xset dpms force off") },
+    { 0, XF86XK_TouchpadToggle,     spawn,                          SHCMD("(synclient | grep 'TouchpadOff.*1' && synclient TouchpadOff=0) || synclient TouchpadOff=1") },
+    { 0, XF86XK_TouchpadOff,        spawn,                          SHCMD("synclient TouchpadOff=1") },
+    { 0, XF86XK_TouchpadOn,         spawn,                          SHCMD("synclient TouchpadOff=0") },
+    { 0, XF86XK_MonBrightnessUp,    spawn,                          SHCMD("xbacklight -inc 15") },
+    { 0, XF86XK_MonBrightnessDown,  spawn,                          SHCMD("xbacklight -dec 15") },
 };
 
 /* button definitions */
